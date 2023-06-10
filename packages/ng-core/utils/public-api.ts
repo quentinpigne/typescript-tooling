eeb73b4c744f87fda6d8ddfbe78c5e627aa3acc2
Quentin Pigné <quentin.pigne@gmail.com>
2023-06-10 13:16:21 +0200

    feat(ng-core): adding connect-form directive

--- packages/ng-core/utils/public-api.ts
+++ packages/ng-core/utils/public-api.ts
@@ -1,2 +1,3 @@
+export * from './src/connect-form.directive';
 export * from './src/ng-var.directive';
 export * from './src/unique-component-id';
---
+++ packages/ng-core/utils/src/connect-form.directive.ts
@@ -0,0 +1,17 @@
+import { Directive, Input } from '@angular/core';
+import { FormGroupDirective } from '@angular/forms';
+
+@Directive({
+  selector: '[connectForm]',
+})
+export class ConnectFormDirective {
+  @Input('connectForm')
+  set data(value: unknown) {
+    if (value) {
+      this.formGroupDirective.form.patchValue(value);
+      this.formGroupDirective.form.markAsPristine();
+    }
+  }
+
+  constructor(private formGroupDirective: FormGroupDirective) {}
+}
